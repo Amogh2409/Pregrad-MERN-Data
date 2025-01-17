e7f42ae8ecb1e3946581771e2526df0818c12e83
Amogh Saxena <amoghsaxena24@gmail.com>
2025-01-17 21:15:26 +0530

    Day - 7 intro to js

---
+++ Day-7/1.js
@@ -0,0 +1,30 @@
+// var age = 17;
+
+// if(age > 18){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// console.log(price)
+
+
+// primitive DATAtypes
+// Number (35)
+// String
+// Bool
+// Null
+// undefined
+// BigInt (34956634504376083474876459874593453406704553408)
+
+
+// lets say we have a lot of things
+// Non-Primitive Types
+
+// Box - object  3-4 strings
+// Array - [1,2,3,4,5,6,7,7] ,  [Candy1, Candy2, Candy3]
+
+
+// Function (instruction)
+//  button - Click  - page refresh
