7b6dbaaedae28e84ffdc00a0c1cb94255251a417
Amogh Saxena <amoghsaxena24@gmail.com>
2025-01-19 20:42:44 +0530

    Arithmetic Operator

---
+++ Day-8/arithmeticOperators.js
@@ -0,0 +1,101 @@
+// +, -, *, /, %
+
+
+// Addition (+)
+
+// let a = 10; 
+// let b = 20;
+// console.log(a + b);
+// let str1 = "Hello";
+// let str2 = "World";
+// console.log(str1 + " " + str2);
+
+
+// Subtraction (-)
+
+// let a = 15;
+// let b = 5;
+// console.log(a - b); // Output: 10
+
+
+// Multiplication (*)
+
+// let a = 4;
+// let b = 3;
+
+// console.log(a * b); // Output: 12
+
+
+// Division (/)
+
+// let a = 20;
+// let b = 4;
+
+// console.log(a / b); // Output: 5
+
+
+// Modulus (%)
+
+// let a = 21;
+// let b = 4;
+
+// console.log(a % b); // Output: 1
+
+
+// Exponentiation (**)
+// Raises the first number to the power of the second number.
+// let a  = 3;
+// let b  = 2;
+// console.log(a ** b);
+
+// let base = 20;
+// let exponent = 9;
+
+// console.log(base ** exponent); // Output: 8
+
+
+// Unary Negation (-)
+// Negates the value of a number.
+// let a = 5;
+
+// console.log(-a); // Output: -5
+
+
+
+// Unary Plus (+)
+
+// Tries to convert a value into a number.
+// let a = "10";
+
+// console.log(+a); // Output: 10 (converted to a number)
+
+
+// let x = 10;
+// let y = 3;
+
+// console.log("Addition:", x + y); 
+// console.log("Subtraction:", x - y);
+// console.log("Multiplication:", x * y); 
+// console.log("Division:", x / y); 
+// console.log("Modulus:", x % y);
+// console.log("Exponentiation:", x ** y);
+
+
+// Order of Operations (Operator Precedence):
+// JavaScript follows the BODMAS rule:
+// Brackets
+// Order (Exponents)
+// Division and Multiplication
+// Addition and Subtraction
+
+
+// let result = 5 + 2 * (10 - 3);
+// console.log(result); 
+
+
+
+//Special Cases
+// console.log(10 / 0); // Output: Infinity
+
+// let a = "hello" * 5;
+// console.log(a) // NAN
