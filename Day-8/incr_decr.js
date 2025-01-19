da32d06b6f1fb992ec37add79f4d9700882b453a
Amogh Saxena <amoghsaxena24@gmail.com>
2025-01-19 21:08:12 +0530

    Increment and Decrement Operators in JS

---
+++ Day-8/incr_decr.js
@@ -0,0 +1,63 @@
+// Increment and Decrement Operators
+// The Increment (++) and Decrement (--) operators are used to increase or decrease the value of a variable by 1
+
+// Increment (++)
+// let x = 5; 
+
+// console.log(x++); //Output: 5 (returns current value, then increments)
+// console.log(x++); //Output: 5 (returns current value, then increments)
+// console.log(x++); //Output: 5 (returns current value, then increments)
+// console.log(x);
+
+// let y = 5
+// console.log(++y);
+// console.log(y);
+
+
+
+// Decrement Operator (--)
+// Post-Decrement (x--): Returns the current value of x and then decrements it.
+// Pre-Decrement (--x): Decrements the value of x first, then returns the updated value.
+
+
+
+// let x = 5;
+
+// console.log(x--); // Output: 5 (returns current value, then decrements)
+// console.log(x);   // Output: 4 (value after decrement)
+
+// let y = 5;
+// console.log(--y); // Output: 4 (decrements first, then returns the new value)
+// console.log(y);   // Output: 4
+
+
+
+
+
+// Operator	Description	           Example	                   Result
+// x++	        Post-Increment    (returns value first)	        x = 5; x++	5
+// ++x	        Pre-Increment     (increments first)	        x = 5; ++x	6
+// x--	        Post-Decrement    (returns value first)	        x = 5; x--	5
+// --x	        Pre-Decrement     (decrements first)	        x = 5; --x	4
+
+
+
+
+// Special Cases
+// let a = 10;
+// // console.log(a);  // 10 
+// let b = a++ + 5;   
+
+// console.log(b);   // 15
+// console.log(a);  // 11 
+
+
+
+//Real-life example
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// for (let j = 5; j > 0; j--) {
+//     console.log(j);
+//  }
