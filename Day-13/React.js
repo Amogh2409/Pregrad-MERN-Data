66d54359bddb9d5ba382cc2b1f03349a16ae077f
Amogh Saxena <amoghsaxena24@gmail.com>
2025-02-01 21:13:51 +0530

    Day -13 (Intro to react, Basic Structure)

---
+++ Day-13/React.js
@@ -0,0 +1,35 @@
+// React is a JavaScript Library known for front-end development (or user interface). It is popular due to its component-based architecture, Single Page Applications (SPAs) and Virtual DOM
+
+// React allows developers to create reusable UI components, making it easier to manage and scale applications.Additionally, React's ecosystem includes tools like React Router for navigation and Redux for state management, enhancing its capabilities.
+
+// Overall, React has become a go-to choice for building modern web applications due to its efficiency and flexibility.Its strong community support and extensive documentation further contribute to its popularity among developers.As a result, many companies and developers prefer using React for their projects.
+
+
+// It uses JSX which combines HTML-like syntax with JavaScript's functionality, making it easy to shift to React from JavaScript.
+
+
+// JSX looks like HTML but is JavaScript. <div> and <h1> are React elements created under the hood.
+// Every tag in JSX (like <h1> or <div>) must be properly closed, including self-closing ones like <img />.
+// All returned elements must be inside one parent (e.g., the <div> here).
+// App is a function that returns the UI. It’s exported so it can be used elsewhere.
+// import React from 'react'; is required to use JSX, as JSX is transformed into JavaScript using React.
+
+
+// Flexibility: React allows you to build high-quality user interfaces across different platforms, thanks to its flexible library approach.
+
+// Great Developer Experience: React makes it easier to write and understand code, offering a smooth development process.
+
+// Strong Support from Facebook: Regular updates, bug fixes, and resources from Facebook ensure React stays up-to-date and reliable.
+
+// React was developed by Facebook in 2011 to improve the performance of their applications and was officially released as an open-source library in 2013.
+
+
+// React introduced concepts like components (reusable UI pieces) and the virtual DOM (a lightweight copy of the real DOM) for optimizing UI 
+
+
+// React uses a Virtual DOM to optimize UI rendering. Instead of updating the entire real DOM directly, React:
+
+// Creates a lightweight copy of the DOM (Virtual DOM).
+// Compares it with the previous version to detect changes (diffing).
+// Updates only the changed parts in the actual DOM (reconciliation), improving performance
+
---
+++ Day-13/my-first-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+        {/* <button onClick={() => alert("This is my First time Trying React !!!!!!!!!!!!")}>Click Me</button> */}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur labore excepturi animi vero, a aliquid, voluptates laboriosam omnis assumenda, eaque nisi facilis numquam ipsam. Non, porro incidunt sit vel ullam dolore animi sapiente odit. Possimus dolore quibusdam tempore nisi autem eius officia hic quam, itaque perferendis repellendus ipsum labore. Vero!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
